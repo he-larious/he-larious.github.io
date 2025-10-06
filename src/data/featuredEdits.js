9a941287d35b56ff448ac00c3d8596f10a27c996
he-larious <helarious99@gmail.com>
2025-10-06 16:28:24 -0700

    update edits

--- src/data/featuredEdits.js
+++ src/data/featuredEdits.js
@@ -1,35 +1,35 @@
 const featuredEdits = [
   {
     title: "AoG | If I Lose Myself",
-    url: "https://drive.google.com/file/d/15y_5nZE-6RDYerH_Lnja-lOqsq_m0h3r/preview"
+    url: "https://drive.google.com/file/d/15y_5nZE-6RDYerH_Lnja-lOqsq_m0h3r/preview",
   },
   {
     title: "2024 Wrapped",
-    url: "https://drive.google.com/file/d/15o-xQy0n1ZxblbLuc8yW9pIm2jLAUZK2/preview"
+    url: "https://drive.google.com/file/d/15o-xQy0n1ZxblbLuc8yW9pIm2jLAUZK2/preview",
   },
   {
     title: "Scream VI | Here",
-    url: "https://www.youtube.com/embed/HkgYZgsi7vo?si=Pqh9k8fpRBGTOEKV"
+    url: "https://www.youtube.com/embed/HkgYZgsi7vo?si=Pqh9k8fpRBGTOEKV",
   },
   {
     title: "Josie Saltzman | thank u, next",
-    url: "https://www.youtube.com/embed/gotlBkPmHXg?si=bcxtzHYpXNeZrU9v"
+    url: "https://www.youtube.com/embed/gotlBkPmHXg?si=bcxtzHYpXNeZrU9v",
   },
   {
     title: "Stranger Things | Con Calma",
-    url: "https://www.youtube.com/embed/yusYeHSawQE?si=fSvoG0HIQQ0FZHRR"
+    url: "https://www.youtube.com/embed/yusYeHSawQE?si=fSvoG0HIQQ0FZHRR",
   },
   {
     title: "Escape Room | This Love",
-    url: "https://www.youtube.com/embed/xY7ROHEOw80?si=VNQEgq2ruK_3ne6V"
+    url: "https://www.youtube.com/embed/xY7ROHEOw80?si=VNQEgq2ruK_3ne6V",
   },
   {
     title: "Sam Carpenter & Kirby Reed | The Exit",
-    url: "https://www.youtube.com/embed/mcoEWgPOjhA?si=uaQimaColmcAA6jF"
+    url: "https://www.youtube.com/embed/mcoEWgPOjhA?si=uaQimaColmcAA6jF",
   },
   {
-    title: "Wednesday & Enid | Dress",
-    url: "https://www.youtube.com/embed/XGpgEqyZkXw?si=VSopog8ZYkfG-IAz"
+    title: "Olivia Rodrigo | Backstabber",
+    url: "https://www.youtube.com/embed/ifw0XAlJBbQ?si=CoWmwWs7KxmEbbki",
   }
 ];
 
